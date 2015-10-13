f90c5ca3ab3d88813a0f57b472763ae8b8cdb17d
C-Stein <caitlin.teter@gmail.com>
2015-10-13 13:39:56 -0700

    Initial solution

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello There! What's your name?");
+            string name = System.Console.ReadLine();
+            System.Console.WriteLine("Hi, " + name);
+            name = name.ToLower();
+            foreach (char letter in name)
+            {
+                string a = "a";
+                bool isVowel = "aefhilmnorsx".IndexOf(letter) >= 0;
+                if (isVowel)
+                {
+                    a = "an";
+                }
+                System.Console.WriteLine("Give me " + a + " " + letter);
+            }
+            System.Console.WriteLine("Presss any key to exit");
+            System.Console.ReadKey();
+        }
+    }
+}
